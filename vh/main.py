a098e7fd31239cb223a23e61b74b52f8308abaf6
Eugene Pankov <john.pankov@gmail.com>
2014-02-15 16:49:55 +0300

    [ui] improvements

--- vh/main.py
+++ vh/main.py
@@ -4,7 +4,7 @@ import subprocess
 from slugify import slugify
 
 from ajenti.api import *
-from ajenti.plugins.main.api import SectionPlugin
+from ajenti.plugins.main.api import SectionPlugin, intent
 from ajenti.ui import on
 from ajenti.ui.binder import Binder
 
@@ -39,109 +39,14 @@ class WebsitesPlugin (SectionPlugin):
         self.append(self.ui.inflate('vh:main'))
 
         self.binder = Binder(None, self)
-        self.find('domains').new_item = lambda c: WebsiteDomain.create('example.com')
-        self.find('ports').new_item = lambda c: WebsitePort.create(80)
-        
-        extensions = BaseExtension.get_classes()
 
         def post_ws_bind(object, collection, item, ui):
-            def create_location():
-                self.binder.update()
-                t = ui.find('create-location-type').value
-                l = WebsiteLocation.create(template=t)
-                l.backend.type = t
-                item.locations.append(l)
-                self.refresh()
-            ui.find('create-location').on('click', create_location)
-
-            # Extensions
-            
-            if hasattr(item, 'extensions'):
-                for ext in item.extensions:
-                    ext._ui_container.delete()
-
-            item.extensions = []
-            for ext in extensions:
-                ext = ext.new(self.ui, item, config=item.extension_configs.get(ext.classname, None))
-                ext._ui_container = self.ui.create('tab', children=[ext], title=ext.name)
-                item.extensions.append(ext)
-                ui.find('tabs').append(ext._ui_container)
-
-            # Root creator
-
-            ui.find('root-not-created').visible = not os.path.exists(item.root)
-
-            def create_root():
-                try:
-                    os.mkdir(item.root)
-                except:
-                    pass
-                subprocess.call(['chown', 'www-data', item.root])
-                self.save()
-
-            ui.find('create-root-directory').on('click', create_root)
-            ui.find('set-path').on('click', self.save)
-
-            # Downloader
-
-            def download():
-                url = ui.find('download-url').value
-                self.save()
-                tmppath = '/tmp/ajenti-v-download'
-                script = 'wget "%s" -O "%s" ' % (url, tmppath)
-                if url.lower().endswith('.tar.gz') or url.lower().endswith('.tgz'):
-                    script += '&& tar xf "%s" -C "%s"' % (tmppath, item.root)
-                elif url.lower().endswith('.zip'):
-                    script += '&& unzip "%s" -d "%s"' % (tmppath, item.root)
-
-                script += ' && chown www-data -R "%s"' % item.root
-                def callback():
-                    self.save()
-                    self.activate()
-                    if os.path.exists(tmppath):
-                        os.unlink(tmppath)
-                    self.context.notify('info', _('Download complete'))
-
-                self.context.launch('terminal', command=script, callback=callback)
-
-            ui.find('download').on('click', download)
-
-
-        def post_ws_update(object, collection, item, ui):
-            for ext in item.extensions:
-                item.extension_configs[ext.classname] = ext.config
-
-        def ws_delete(ws, collection):
-            for ext in ws.extensions:
-                try:
-                    ext.on_destroy()
-                except Exception, e:
-                    logging.error(str(e))
-            collection.remove(ws)
-            self.save()
-
-        def post_location_bind(object, collection, item, ui):
-            ui.find('backend-params').empty()
-            ui.find('backend-params').append(self.ui.inflate('vh:main-backend-params-%s' % item.backend.type))
-            item.backend.__binder = Binder(item.backend, ui.find('backend-params'))
-            item.backend.__binder.populate()
-
-        def post_location_update(object, collection, item, ui):
-            item.backend.__binder.update()
+            def manage():
+                self.context.launch('v:manage-website', website=item)
+            ui.find('manage').on('click', manage)
 
         self.find('websites').post_item_bind = post_ws_bind
-        self.find('websites').post_item_update = post_ws_update
-        self.find('websites').delete_item = ws_delete
 
-        self.find('locations').post_item_bind = post_location_bind
-        self.find('locations').post_item_update = post_location_update
-
-        self.find('create-location-type').labels = []
-        self.find('create-location-type').values = []
-        for g in sorted(ApplicationGatewayComponent.get_classes(), key=lambda x: x.title):
-            self.find('create-location-type').labels.append(g.title)
-            self.find('create-location-type').values.append(g.id)
-            
         self.binder.setup(self.manager.config)
 
     @on('new-website', 'click')
@@ -164,8 +69,6 @@ class WebsitesPlugin (SectionPlugin):
         self.binder.populate()
 
     def on_page_load(self):
-        for ext in BaseExtension.get_classes():
-            ext.selftest()
         self.refresh()
 
     def refresh(self):
@@ -175,6 +78,143 @@ class WebsitesPlugin (SectionPlugin):
     @on('save', 'click')
     def save(self):
         self.binder.update()
+        self.context.endpoint.send_progress(_('Saving changes'))
+        self.manager.save()
+        self.context.endpoint.send_progress(_('Applying changes'))
+        self.manager.update_configuration()
+        self.refresh()
+        self.context.notify('info', _('Saved'))
+
+
+@plugin
+class WebsiteEditorPlugin (SectionPlugin):
+    def init(self):
+        self.title = 'Website editor'
+        self.icon = 'globe'
+        self.category = 'Web'        
+        self.hidden = True
+
+        self.manager = VHManager.get()
+        self.binder = Binder(None, self)
+        
+        self.append(self.ui.inflate('vh:main-website'))
+        self.find('domains').new_item = lambda c: WebsiteDomain.create('example.com')
+        self.find('ports').new_item = lambda c: WebsitePort.create(80)
+
+        def post_location_bind(object, collection, item, ui):
+            ui.find('backend-params').empty()
+            ui.find('backend-params').append(self.ui.inflate('vh:main-backend-params-%s' % item.backend.type))
+            item.backend.__binder = Binder(item.backend, ui.find('backend-params'))
+            item.backend.__binder.populate()
+
+        def post_location_update(object, collection, item, ui):
+            item.backend.__binder.update()
+
+        self.find('locations').post_item_bind = post_location_bind
+        self.find('locations').post_item_update = post_location_update
+
+        self.find('create-location-type').labels = []
+        self.find('create-location-type').values = []
+        for g in sorted(ApplicationGatewayComponent.get_classes(), key=lambda x: x.title):
+            self.find('create-location-type').labels.append(g.title)
+            self.find('create-location-type').values.append(g.id)
+
+    @intent('v:manage-website')
+    def on_launch(self, website=None):
+        self.activate()
+        self.website = website
+        self.binder.setup(self.website)
+        self.binder.populate()
+
+        for ext in BaseExtension.get_classes():
+            ext.selftest()
+
+        extensions = BaseExtension.get_classes()
+
+        def create_location():
+            self.binder.update()
+            t = self.find('create-location-type').value
+            l = WebsiteLocation.create(template=t)
+            l.backend.type = t
+            self.website.locations.append(l)
+            self.refresh()
+        self.find('create-location').on('click', create_location)
+
+        # Extensions
+        
+        if hasattr(self.website, 'extensions'):
+            for ext in self.website.extensions:
+                ext._ui_container.delete()
+
+        self.website.extensions = []
+        for ext in extensions:
+            ext = ext.new(self.ui, self.website, config=self.website.extension_configs.get(ext.classname, None))
+            ext._ui_container = self.ui.create('tab', children=[ext], title=ext.name)
+            self.website.extensions.append(ext)
+            self.find('tabs').append(ext._ui_container)
+
+        # Root creator
+        self.find('root-not-created').visible = not os.path.exists(self.website.root)
+
+        def create_root():
+            try:
+                os.mkdir(self.website.root)
+            except:
+                pass
+            subprocess.call(['chown', 'www-data', self.website.root])
+            self.save()
+
+        self.find('create-root-directory').on('click', create_root)
+        self.find('set-path').on('click', self.save)
+
+        # Downloader
+
+        def download():
+            url = self.find('download-url').value
+            self.save()
+            tmppath = '/tmp/ajenti-v-download'
+            script = 'wget "%s" -O "%s" ' % (url, tmppath)
+            if url.lower().endswith('.tar.gz') or url.lower().endswith('.tgz'):
+                script += '&& tar xf "%s" -C "%s"' % (tmppath, self.website.root)
+            elif url.lower().endswith('.zip'):
+                script += '&& unzip "%s" -d "%s"' % (tmppath, self.website.root)
+
+            script += ' && chown www-data -R "%s"' % self.website.root
+            def callback():
+                self.save()
+                self.activate()
+                if os.path.exists(tmppath):
+                    os.unlink(tmppath)
+                self.context.notify('info', _('Download complete'))
+
+            self.context.launch('terminal', command=script, callback=callback)
+
+        self.find('download').on('click', download)
+
+    @on('go-back', 'click')
+    def on_go_back(self):
+        WebsitesPlugin.get().activate()
+
+    @on('destroy', 'click')
+    def on_destroy(self):
+        for ext in self.website.extensions:
+            try:
+                ext.on_destroy()
+            except Exception, e:
+                logging.error(str(e))
+        self.save()
+        self.on_go_back()
+
+    def refresh(self):
+        self.binder.unpopulate().populate()
+        
+    @on('save', 'click')
+    def save(self):
+        self.binder.update()
+
+        for ext in self.website.extensions:
+            self.website.extension_configs[ext.classname] = ext.config
+
         self.context.endpoint.send_progress(_('Saving changes'))
         self.manager.save()
         self.context.endpoint.send_progress(_('Applying changes'))
