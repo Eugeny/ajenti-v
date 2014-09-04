e02f804bd50df7a69ebda625987b885c28da02b8
Eugene Pankov <john.pankov@gmail.com>
2014-09-04 17:32:27 +0300

    fixed Eugeny/ajenti/#568

--- vh-mail/api.py
+++ vh-mail/api.py
@@ -223,9 +223,11 @@ class MailEximCourierBackend (MailBackend):
             'tls_pem': pem_path,
         })
 
+        if os.path.exists(courier_authsocket):
+            os.chmod(self.courier_authsocket, 0755)
+        
         socketdir = os.path.split(self.courier_authsocket)[0]
         if os.path.exists(socketdir):
-            os.chmod(self.courier_authsocket, 0755)
             os.chmod(socketdir, 0755)
 
         # Domain entries ----------------------------
