dcd07ffe9a186ba5561bec45fe6febf37499511d
Alessio Dionisi <a.dionisi@shinyweb.org>
2015-12-23 22:59:53 +0000

    PHP7.0 FPM Plugin

---
+++ vh-php7.0-fpm/__init__.py
@@ -0,0 +1,24 @@
+import ajenti
+from ajenti.api import *
+from ajenti.plugins import *
+from ajenti.util import platform_select
+
+
+info = PluginInfo(
+    title='Ajenti VH - PHP7.0-FPM Support',
+    icon='globe',
+    dependencies=[
+        PluginDependency('vh'),
+        PluginDependency('services'),
+        BinaryDependency(platform_select(
+            default='php-fpm7.0'
+        )),
+    ],
+)
+
+
+def init():
+    from ajenti.plugins.vh import destroyed_configs
+    destroyed_configs.append('php7.0-fpm')
+
+    import phpfpm
